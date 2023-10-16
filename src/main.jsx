41f68d824b78374ac50f7ce39e8f332d969adcdc
greg <gregory.stepanov@live.ru>
2023-10-16 17:09:37 +0300

    get random char

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./components/app/App.jsx"
-import MarvelServices from "./services/MarvelServices.js";
 import './style/style.scss';
-
-const marvelServices = new MarvelServices()
-
-marvelServices.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
